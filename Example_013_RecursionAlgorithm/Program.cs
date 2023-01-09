a6a5f974585575c02488479d0ab8a398afedcd43
Mihail <horunzhy.m@yandex.ru>
2023-01-09 13:08:59 +0300

    Recursion 13

--- Example_013_RecursionAlgorithm/Program.cs
+++ Example_013_RecursionAlgorithm/Program.cs
@@ -1,9 +1,9 @@
-﻿            //
+﻿   /*         //
 string[,] table = new string[2, 5];
 table[1, 2] = "слово";
 // String.Empty
 // table[0,0]  table[0,1]...  table[0,4] 
-/* table[1,0]  table[1,1]...  table[1,4]
+ table[1,0]  table[1,1]...  table[1,4]
 
 table[1, 2] = "слово";
 
@@ -64,7 +64,7 @@ int[,] matrix = new int[3, 4];
 
 PrintArray(matrix);
 
-*/// Вариант 4 (Добавим вывод случаййных числел)
+// Вариант 4 (Добавим вывод случаййных числел)
 
 void PrintArray(int[,] matr)
 {
@@ -99,3 +99,121 @@ int[,] matrix = new int[3, 4];
 PrintArray(matrix);
 FillArray(matrix);
 PrintArray(matrix);
+
+// Вывод картинки (палец вверх)  
+
+int[,] pic = new int[,]
+{
+    {0, 0, 1, 1, 1, 1, 0, 0, },  // Рисунка в двоичном коде
+    {0, 1, 0, 0, 0, 0, 1, 0, },
+    {1, 0, 1, 0, 0, 1, 0, 1, },
+    {1, 0, 0, 0, 0, 0, 0, 1, },
+    {1, 0, 1, 0, 0, 1, 0, 1, },
+    {1, 0, 0, 1, 1, 0, 0, 1, },
+    {0, 1, 0, 0, 0, 0, 1, 0, },
+    {0, 0, 1, 1, 1, 1, 0, 0, },
+};   
+
+void PrintImage(int[,] image)
+{
+
+    for (int i = 0; i < image.GetLength(0); i++)
+    {
+        for (int j = 0; j < image.GetLength(1); j++)
+        {
+             //Console.Write($"{image[i, j]} ");
+             if(image[i, j] == 0) Console.Write($" ");
+             else Console.Write($"+");
+        }
+        Console.WriteLine();
+
+    }    
+}    
+
+void FillImage(int row, int col)
+{
+    if(pic[row, col] == 0)
+    {
+        pic[row, col] = 1;
+        FillImage(row -1, col);
+        FillImage(row, col -1);
+        FillImage(row +1, col);
+        FillImage(row, col + 1);
+    }
+}
+PrintImage(pic);
+FillImage(3, 3);
+PrintImage(pic);
+                                    
+
+
+// Факториал.
+
+int Factorial(int n)
+{
+    // 1! = 1
+    // 0! = 1
+    if (n == 1) return 1;
+    else return n * Factorial(n-1);
+}
+
+Console.WriteLine(Factorial(5)); // 1*2*3=6 
+                                                        
+// Вариант 2
+
+int Factorial(int n)
+{
+    if (n == 1) return 1;
+    else return n * Factorial(n-1);
+}
+for (int i = 1; i < 40; i++)
+{
+    Console.WriteLine(Factorial(i)); 
+}
+                          
+
+// Вариант 3
+
+int Factorial(int n)
+{
+    if (n == 1) return 1;
+    else return n * Factorial(n-1);
+}
+for (int i = 1; i < 40; i++)
+{
+    Console.WriteLine($"{i}! = {Factorial(i)}"); // Показываем какой факториал.
+}                                                 //Число 17 не помещается в тип 'int' заменим на 'double'                 
+
+ //меняем на double
+
+double Factorial(int n)
+{
+    if (n == 1) return 1;
+    else return n * Factorial(n-1);
+}
+for (int i = 1; i < 40; i++)
+{
+    Console.WriteLine($"{i}! = {Factorial(i)}"); 
+}                                              
+
+// ЧИСЛА ФИБОНАЧИ
+
+// f(1) = 1
+// f(2) = 1
+// f(3) = 2
+// f(4) = 3
+// f(5) = 5
+// f(n) = f(n-1) + f(n-2)
+
+int Fibonacci(int n)
+{
+    if(n == 1 || n == 2) return 1;
+    else return Fibonacci(n-1) + Fibonacci(n-2);
+}
+for (int i = 1; i < 50; i++)
+{
+    Console.WriteLine($"f({i}) = {Fibonacci(i)}");
+}
+                                             */
+//Задача (Как обойти дерикторию) 
+
