35ad35ad6cff21a49b42815294a64a8eab2d8f80
Mihail <horunzhy.m@yandex.ru>
2023-01-23 18:35:06 +0300

    Lesson 0.1

---
+++ Example_Lesson_/0.1/Program.cs
@@ -0,0 +1,15 @@
+﻿int n = 1;
+void FindWords(string alphabet, char[] word, int length = 0) //метод-переданный строкой alp, масс из букв word, 
+// собирается длинна слов leng из букв.  
+{
+    if (length == word.Length)
+    {
+        Console.WriteLine($"{n++} {new String(word)}"); return;
+    }
+    for (int i = 0; i < alphabet.Length; i++)
+    {
+        word[length] = alphabet[i];
+        FindWords(alphabet, word, length + 1);
+    }
+}
+FindWords("аисв", new char[1]);
